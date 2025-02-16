bc60a6758f7ca441a61d6a4a820b3001e0ca9019
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2025-02-16 14:47:27 +0000

    initial commit

---
+++ mcp-host/src/mcp_host/main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Stateless MCP Host",
+    description="Stateless MCP Host",
+    version="0.1.0",
+)
---
+++ mcp-server/README.md
@@ -0,0 +1,3 @@
+# MCP server
+
+This is the MCP server connected to by claude desktop or any other client.
